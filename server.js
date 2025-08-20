6555f3efdbf3ba9959eb483f67c43a98b5f41241
LA4th <lorenzolaforteza311@gmail.com>
2025-08-21 07:55:47 +0800

    Adding the cli running server

--- server.js
+++ server.js
@@ -14,9 +14,8 @@ const server = http.createServer((req, res) => {
   // POST DATA (Create notes)
   else if(req.method === "POST" && req.url === "/notes") {
     let body = "";
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
+
+    req.on("data", (chunck) => {body += chunck});
     req.on("end", () => {
       try {
         const note = JSON.parse(body);
@@ -24,41 +23,44 @@ const server = http.createServer((req, res) => {
         notes.push(note);
 
         res.writeHead(201);
-        res.end(JSON.stringify({message: "Add notes!", note}));
-      } catch(err) {
-        res.writeHead(404);
-        res.end(JSON.stringify({erro: "Invalid JSON format"}));
+        res.end(JSON.stringify({message: "Note added!", note}));
+      }
+      catch(err) {
+        res.writeHead(400);
+        res.end(JSON.stringify({error: "Invalid JSON format!"}));
       }
     });
   }
   // PUT DATA (Update note "PUT /notes/:id")
   else if(req.method === "PUT" && req.url.startsWith("/notes/")) {
-    const id =parseInt(req.url.split("/")[2]);
+    const id = parseInt(req.url.split("/")[2]);
     let body = "";
 
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
+    req.on("data", (chunck) => {body += chunck});
     req.on("end", () => {
       try {
         const updated = JSON.parse(body);
-        let note = notes.find((n) => {
-          n.id === id;
-        });
+        let note = notes.find((n) => {n.id === id});
+        notes.push(note);
+
         if(!note) {
           res.writeHead(404);
-          return res.end(JSON.stringify({error: "Note not found"}));
+          return res.end({error: "Note not found!"});
         }
         note.title = updated.title || note.title;
         note.content = updated.content || note.content;
-        
-        res.writeHead(200);
+
+        res.writeHead(200)
         res.end(JSON.stringify({message: "Note updated!"}));
-      } 
+      }
       catch(err) {
-        res.writeHead(404);
-        res.end(JSON.stringify({error: "Invalid JSON format"}));
+        res.writeHead(400);
+        res.end(JSON.stringify({error: "Invalid JSON format!"}));
       }
     });
   }
 });
+
+server.listen(port, () => {
+  console.log(`Server running: ${host}`);
+})
